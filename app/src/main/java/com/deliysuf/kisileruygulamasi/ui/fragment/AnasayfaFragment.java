0910c6ef89c8b15e708a9efb6414909f1fb6db47
yusuf çetin <yusufpau@outlook.com>
2022-12-06 18:27:02 +0300

    kisiler

---
+++ app/src/main/java/com/deliysuf/kisileruygulamasi/ui/fragment/AnasayfaFragment.java
@@ -0,0 +1,23 @@
+package com.deliysuf.kisileruygulamasi.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.deliysuf.kisileruygulamasi.R;
+
+
+public class AnasayfaFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_anasayfa, container, false);
+    }
+}
---
+++ app/src/main/java/com/deliysuf/kisileruygulamasi/ui/fragment/KisiDetayFragment.java
@@ -0,0 +1,26 @@
+package com.deliysuf.kisileruygulamasi.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.deliysuf.kisileruygulamasi.R;
+
+
+public class KisiDetayFragment extends Fragment {
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_kisi_detay, container, false);
+    }
+}
